1c8b2b7bd8f07e63ce0f89c121c4763c02031ea0
Shreyas <shreyas.ghule@hoonartek.com>
2025-06-12 18:59:02 +0530

    Config file added

---
+++ config/setting.py
@@ -0,0 +1,8 @@
+settings={
+    "Application Name": "DQ Solution",
+    "input_data_path": "data/input/",
+    "processed_data_path" : "data/processed/",
+    "output_path": "data/output/",
+    "customer_dq_rules": "config/customer_dq_rules/",
+    "log_path": "log/"
+}
